4e181e25253f6c1b12fc9e4058808dee1f9d9267
AmirAli <amiraliamb@outlook.com>
2024-12-12 20:09:27 +0330

    added support for the Persian language

--- abroo_project/settings.py
+++ abroo_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -108,13 +109,26 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
+
+LANGUAGES = [
+    ('en', 'English'),
+    ('fa', 'Persian'),
+    # Add other languages if needed
+]
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
 
 
 # Static files (CSS, JavaScript, Images)
--- bot/bot.py
+++ bot/bot.py
@@ -1,26 +1,41 @@
 import os
-from telegram import Update
+from telegram import Update, User
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
 from bot.services import get_or_create_user_profile
+from django.utils.translation import gettext as _
+from django.utils import translation
+import abroo_project.settings as settings
 
 load_dotenv()
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
+async def activate_user_language(user: User):
+    user_language = user.language_code
+    if user_language in dict(settings.LANGUAGES).keys():
+        translation.activate(user_language)
+    else:
+        translation.activate(settings.LANGUAGE_CODE)
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
+    await activate_user_language(user)
     # Update or create the user profile
     user_profile, created = await get_or_create_user_profile(user)
     
     if created:
-        greeting = f"Hello, {user.first_name}! Nice to meet you."
+        greeting = _("Hello, {first_name}! Nice to meet you.").format(first_name=user.first_name)
     else:
-        greeting = f"Welcome back, {user.first_name}!"
+        greeting = _("Welcome back, {first_name}!").format(first_name=user.first_name)
 
     await update.message.reply_text(greeting)
+    translation.deactivate()
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    await activate_user_language(user)
     await update.message.reply_text("I am here to help you.")
+    translation.deactivate()
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
